8860222d20fd53b9f3313640283131b0915e9a82
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-12 02:09:22 +0100

    src/app/pages/create-profile/create-profile.ts

--- src/app/pages/create-profile/create-profile.ts
+++ src/app/pages/create-profile/create-profile.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { WorkRoleService } from '../../services/work-role';
 
 @Component({
   standalone: true,
   selector: 'app-create-profile',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './create-profile.html',
   styleUrls: ['./create-profile.css'],
 })
-export class CreateProfile {
+export class CreateProfile implements OnInit {
   profile = {
     firstname: '',
     lastname: '',
@@ -17,11 +19,21 @@ export class CreateProfile {
     gender: '',
     job_role: '',
     department: '',
-    address: ''
+    address: '',
   };
+
+  constructor(private router: Router, private workRole: WorkRoleService) {}
+
+  ngOnInit() {
+    if (!this.workRole.isAdmin()) {
+      alert('Access denied. Only admins can create employee profiles.');
+      this.router.navigate(['/homepage']);
+    }
+  }
 
   saveProfile() {
     console.log('Profile saved:', this.profile);
     alert('Profile created successfully!');
+    this.router.navigate(['/signin']);
   }
 }
