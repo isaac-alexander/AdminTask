22261a1d0217fc3e5fb4e4c27b8fc68183e99a32
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-12 02:10:45 +0100

    src/app/pages/sign-in/sign-in.ts

--- src/app/pages/sign-in/sign-in.ts
+++ src/app/pages/sign-in/sign-in.ts
@@ -1,27 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { WorkRoleService } from '../../services/work-role';
 
 @Component({
   standalone: true,
   selector: 'app-sign-in',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './sign-in.html',
   styleUrls: ['./sign-in.css'],
 })
 export class SignIn {
   email = '';
   password = '';
+  role: 'admin' | 'employee' | '' = '';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private workRole: WorkRoleService) {}
 
   signIn() {
-    if (this.email && this.password) {
-      console.log('Signed in as:', this.email);
-      this.router.navigate(['/create-profile']);
-    } else {
-      alert('Please enter both email and password.');
+    if (!this.email || !this.password || !this.role) {
+      alert('Email, password and role are required.');
+      return;
     }
+
+    this.workRole.currentRole = this.role;
+    this.router.navigate(['/homepage']);
   }
 }
