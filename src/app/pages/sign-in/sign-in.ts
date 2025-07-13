3c700e12c4efdab5e5de10736e9e1418692eb6cd
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-13 02:26:57 +0100

    src/app/pages/sign-in/sign-in.ts

--- src/app/pages/sign-in/sign-in.ts
+++ src/app/pages/sign-in/sign-in.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
-import { WorkRoleService } from '../../services/work-role';
+import { SignInService } from '../../services/sign-in.service';
+import { SignInUser } from '../../SignInUser';
+import { SignInResponse } from '../../SignInResponse';
 
 @Component({
   standalone: true,
@@ -14,17 +16,38 @@ import { WorkRoleService } from '../../services/work-role';
 export class SignIn {
   email = '';
   password = '';
-  role: 'admin' | 'employee' | '' = '';
+  error = '';
 
-  constructor(private router: Router, private workRole: WorkRoleService) {}
+  @Output() loggedIn = new EventEmitter<SignInResponse['data']>();
+res: any;
+
+
+  constructor(private router: Router, private signInService: SignInService) { }
 
   signIn() {
-    if (!this.email || !this.password || !this.role) {
+    if (!this.email || !this.password) {
       alert('Email, password and role are required.');
       return;
     }
 
-    this.workRole.currentRole = this.role;
-    this.router.navigate(['/homepage']);
+    const userData: SignInUser = { email: this.email, password: this.password };
+
+    this.signInService.signIn(userData).subscribe({
+      next: (res) => {
+        console.log('response', res);
+        this.error = '';
+
+        //emit to homepage
+        //navigate to homepage
+        this.loggedIn.emit(res.data)
+        this.router.navigate(['/homepage']);
+
+      },
+      error: (err) => {
+        console.log('invalid crede', err);
+        this.error = 'invalid email or password';
+      }
+    },)
+
   }
 }
