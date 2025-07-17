5e99fcdb63b5f91ed704af18495b05573ca294a8
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-17 02:04:52 +0100

    src/app/pages/view-article/

---
+++ src/app/pages/view-article/view-article.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Article } from '../../Article';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-view-article',
+  imports: [CommonModule, FormsModule, RouterModule],
+  templateUrl: './view-article.html',
+  styleUrl: './view-article.css'
+})
+
+export class ViewArticle {
+  article: Article | null = null;
+  newComment: string = '';
+
+  constructor(private route: ActivatedRoute) {
+    const articleId = Number(this.route.snapshot.paramMap.get('id'));
+
+    const stored = localStorage.getItem('articles');
+    const articles: Article[] = stored ? JSON.parse(stored) : [];
+
+    this.article = articles.find(a => a.id === articleId) || null;
+  }
+
+  addComment() {
+    if (!this.article || !this.newComment.trim()) return;
+
+    this.article.comments.push(this.newComment.trim());
+
+    const stored = localStorage.getItem('articles');
+    let articles: Article[] = stored ? JSON.parse(stored) : [];
+
+    const index = articles.findIndex(a => a.id === this.article!.id);
+    if (index !== -1) {
+      articles[index] = this.article;
+      localStorage.setItem('articles', JSON.stringify(articles));
+    }
+
+    this.newComment = '';
+  }
+}
