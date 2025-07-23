b0196462113e056e8e594f707f7a75e8ecab18ab
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-23 02:04:24 +0100

    src/app/pages/post-article/post-article.ts

--- src/app/pages/post-article/post-article.ts
+++ src/app/pages/post-article/post-article.ts
@@ -18,6 +18,10 @@ export class PostArticle {
     title: '',
     content: '',
   };
+
+  articles: (Article & { id: number })[] = [];
+  editingPost: (Article & { id: number }) | null = null;
+
   constructor(private articleService: ArticleService) { }
 
 
@@ -27,13 +31,57 @@ export class PostArticle {
     this.articleService.postArticle(this.article).subscribe({
       next: (res: ArticleResponse) => {
         console.log('Article posted:', res.data);
+
+        const newArticle: Article & { id: number } = {
+          id: res.data.articleId,
+          title: res.data.title,
+          content: res.data.article,
+        };
+
+        this.articles.unshift(newArticle);
         this.article = { title: '', content: '' };
       },
       error: (err) => {
         console.error('Failed to post article:', err);
-      }
+      },
     });
   }
 
-  
+  editArticle(article: Article & { id: number }) {
+    this.article = { title: article.title, content: article.content };
+    this.editingPost = article;
+  }
+
+  updateArticle() {
+    if (!this.article.title || !this.article.content || !this.editingPost) return;
+
+    const articleToUpdate = {
+      id: this.editingPost!.id,
+      title: this.article.title,
+      content: this.article.content
+    };
+
+    this.articleService.updateArticle(articleToUpdate).subscribe({
+      next: (res: ArticleResponse) => {
+        console.log('Article updated successfully:', res.data);
+
+        const index = this.articles.findIndex(a => a.id === this.editingPost!.id);
+        if (index > -1) {
+          this.articles[index] = {
+            id: this.editingPost!.id,
+            title: this.article.title,
+            content: this.article.content
+          };
+        }
+
+        this.article = { title: '', content: '' };
+        this.editingPost = null;
+      },
+      error: (err) => {
+        console.error('Failed to update article:', err);
+      },
+    });
+  }
+
+
 }
